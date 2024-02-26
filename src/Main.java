95b9f35b0cea3d975016d8ccf5352558434969ff
omurbekavalbekov <omurbekavalbekov9@gmail.com>
2024-02-26 10:14:00 +0800

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int seconds = 100000;
+        int secondsInMinute = 60;
+        int secondsInHours = secondsInMinute * 60;
+        int secondsInDays = secondsInHours * 24;
+
+        int days = seconds / secondsInDays;
+        int hours = (seconds % secondsInDays) / secondsInHours;
+        int minutes = ((seconds % secondsInDays) % secondsInHours) / secondsInMinute;
+        int leftSeconds = ((seconds% secondsInDays) % secondsInHours) % secondsInMinute;
+
+        System.out.println("Days = " + days);
+        System.out.println("Hours = " + hours);
+        System.out.println("Minutes = " + minutes);
+        System.out.println("Seconds = " + leftSeconds);
+
+
     }
 }
